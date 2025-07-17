e10e3944ff8595828b5110aa838868e618c53ace
TaufiqMP <taufiqmaulanapr@gmail.com>
2025-07-17 18:59:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://suitmedia-backend.suitdev.com',
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
     },
   },
